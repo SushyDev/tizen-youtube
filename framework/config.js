f49c4f7833b064bb39d18412cc487a7ee9a55378
Sushy <43114803+SushyDev@users.noreply.github.com>
2026-09-12 00:59:16 +0200

    remove migration code (#44)

--- framework/config.js
+++ framework/config.js
@@ -85,30 +85,7 @@ function readStoredSettings() {
   }
 }
 
-const RETIRED_STARTUP_KEYS = ['launchToOnStartup', 'reloadHomeOnStartup'];
-
-function startPageOf(launchTo) {
-  try {
-    const browseId = JSON.parse(launchTo).browseEndpoint.browseId;
-    return typeof browseId === 'string' && browseId !== 'FEtopics' ? browseId : '';
-  } catch (e) {
-    return '';
-  }
-}
-
-function migrateStartPage(settings) {
-  const current = Object.keys(settings)
-    .filter((key) => RETIRED_STARTUP_KEYS.indexOf(key) === -1)
-    .reduce((kept, key) => Object.assign({}, kept, { [key]: settings[key] }), {});
-
-  return settings.reloadHomeOnStartup === true && settings.startupPage === undefined
-    ? Object.assign({}, current, { startupPage: startPageOf(settings.launchToOnStartup) })
-    : current;
-}
-
-const stored = migrateStartPage(readStoredSettings());
-
-const localConfig = Object.assign({}, defaultConfig, stored);
+const localConfig = Object.assign({}, defaultConfig, readStoredSettings());
 
 export function configRead(key) {
   return localConfig[key] !== undefined ? localConfig[key] : defaultConfig[key];
